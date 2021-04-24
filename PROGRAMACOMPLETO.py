1115b83dbf94e693c949844fa92c891d4b577bdb
thurra97 <81187889+thurra97@users.noreply.github.com>
2021-04-24 17:44:36 -0300

    Alteração programa  - 2

--- PROGRAMACOMPLETO.py
+++ PROGRAMACOMPLETO.py
@@ -1,9 +1,11 @@
-import EP2_1
+lista = ['A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','K♠','Q♠','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','Q♣','K♣','J♣','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','Q♦','K♦','J♦']
+
 import EP2_2
 import EP2_3
 import EP2_4
 import EP2_5
 import EP2_6
 
-print lista
-carta_desejada = input('Escolha a carta desejada:')
+for n, item in enumerate(lista):
+    print(n+1, item)
+carta_desejada = int(input('Digite o número da carta desejada:'))
