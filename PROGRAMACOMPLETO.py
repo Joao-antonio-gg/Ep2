d56d898c5d3f578a7c19abe5dc03dff1f2ad816f
joao <joaoagg@al.insper.edu.br>
2021-04-26 18:05:34 -0300

    Edição João

--- PROGRAMACOMPLETO.py
+++ PROGRAMACOMPLETO.py
@@ -1,11 +1,16 @@
-lista = ['A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','K♠','Q♠','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','Q♣','K♣','J♣','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','Q♦','K♦','J♦']
 
+import EP2_1
 import EP2_2
 import EP2_3
 import EP2_4
 import EP2_5
 import EP2_6
 
-for n, item in enumerate(lista):
+baralho = EP2_1.cria_baralho()
+
+for n, item in enumerate(baralho):
     print(n+1, item)
+    
 carta_desejada = int(input('Digite o número da carta desejada:'))
+
+verifica = EP2_4.lista_movimentos_possiveis(baralho,carta_desejada)
