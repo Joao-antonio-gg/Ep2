833e73e06bbcd8023489f5c4c0661f096cce9a72
thurra97 <81187889+thurra97@users.noreply.github.com>
2021-04-28 18:21:41 -0300

    Alteração Arthur

--- PROGRAMACOMPLETO.py
+++ PROGRAMACOMPLETO.py
@@ -1,11 +1,15 @@
-lista = ['A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','K♠','Q♠','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','Q♣','K♣','J♣','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','Q♦','K♦','J♦']
-
+import random
+def cria_baralho():
+    lista = ['A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','K♠','Q♠','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','Q♣','K♣','J♣','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','Q♦','K♦','J♦']
+    random.shuffle(lista)
+    return lista
 import EP2_2
 import EP2_3
 import EP2_4
 import EP2_5
 import EP2_6
 
-for n, item in enumerate(lista):
-    print(n+1, item)
+lista = cria_baralho
+for n, item in enumerate(cria_baralho()):
+    print(n + 1, item)
 carta_desejada = int(input('Digite o número da carta desejada:'))
