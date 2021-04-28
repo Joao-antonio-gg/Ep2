cedcb7367e73d6b4979ade972e07fa0e3218b664
thurra97 <81187889+thurra97@users.noreply.github.com>
2021-04-28 18:51:45 -0300

    Edição Joao + Arthur

--- PROGRAMACOMPLETO.py
+++ PROGRAMACOMPLETO.py
@@ -9,6 +9,29 @@ import EP2_4
 import EP2_5
 import EP2_6
 
+print('')
+print('Paciência Acordeão')
+print('')
+print('=====================')
+print('')
+print('O objetivo deste jogo é colocar todas as cartas em uma mesma pilha')
+print('')
+print('Existem apenas dois movimentos possíveis:')
+print('')
+print('1 - Empilhar uma carta sobre a carta imediatamente anterior.')
+print('2 - Empilhar uma carta sobre a terceira carta anterior.')
+print('')
+print('Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:')
+print('')
+print('1 - As duas cartas possuem o mesmo valor')
+print('     ou    ')
+print('2 - As duas cartas possuem o mesmo naipe ')
+print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada')
+print('')
+print('Boa sorte e Bom Jogo!')
+print('')
+print('')
+
 for n, item in enumerate(cria_baralho()):
     
     print(n + 1, item)
