84460e9774cf3cdd6ed923b068d3da693f6c3409
joao <joaoagg@al.insper.edu.br>
2021-05-04 19:56:59 -0300

    João + Arthur

--- PROGRAMACOMPLETO.py
+++ PROGRAMACOMPLETO.py
@@ -36,41 +36,52 @@ for n, item in enumerate(cria_baralho()):
     y = n + 1
     print(n + 1, item)
 
-while len(cria_baralho()) > 1 and EP2_6.possui_movimentos_possiveis(cria_baralho()) == True:
+Restart = True    
+while Restart :
 
-    carta_desejada = int(input('Digite o número da carta desejada:'))
+    while len(cria_baralho()) > 1 and EP2_6.possui_movimentos_possiveis(cria_baralho()) == True:
 
-    # if carta_desejada > len(cria_baralho()) or carta_desejada <= 0:
-    #     print('Essa carta é invalida, digite uma carta valida.')
-    #     carta_desejada = int(input('Digite o número da carta desejada:'))
-    
-    indice_carta = carta_desejada - 1
-    verifica_funcionamento = EP2_4.lista_movimentos_possiveis(cria_baralho(),indice_carta)
-    indice_igual_carta = cria_baralho()[indice_carta]
+        carta_desejada = int(input('Digite o número da carta desejada:'))
 
-    if  verifica_funcionamento == []:
-        Try_again = int(input('Essa carta não pode ser empilhada, escolha outra:'))
-        indice_try_again = Try_again - 1
-        verifica_funcionamento = EP2_4.lista_movimentos_possiveis(cria_baralho(),indice_try_again)
-    
-    if verifica_funcionamento == [1] :
-        for n, item in enumerate(EP2_5.empilha(cria_baralho(), indice_carta , indice_carta - 1)):
-            print (n + 1, item)
+        # if carta_desejada > len(cria_baralho()) or carta_desejada <= 0:
+        #     print('Essa carta é invalida, digite uma carta valida.')
+        #     carta_desejada = int(input('Digite o número da carta desejada:'))
         
-    
-    
-    if verifica_funcionamento == [3]:
-        for n, item in enumerate(EP2_5.empilha(cria_baralho(), indice_carta , indice_carta - 3)):
-            print (n + 1, item)
+        indice_carta = carta_desejada - 1
+        verifica_funcionamento = EP2_4.lista_movimentos_possiveis(cria_baralho(),indice_carta)
+        indice_igual_carta = cria_baralho()[indice_carta]
+
+        if  verifica_funcionamento == []:
+            Try_again = int(input('Essa carta não pode ser empilhada, escolha outra:'))
+            indice_try_again = Try_again - 1
+            verifica_funcionamento = EP2_4.lista_movimentos_possiveis(cria_baralho(),indice_try_again)
         
-    
-    if verifica_funcionamento == [1,3]:
-        Qual_carta = int(input('Qual carta deseja empilhar ?'))
-        while Qual_carta == indice_carta - 3 or Qual_carta == indice_carta - 1 :
-            for n, item in enumerate(EP2_5.empilha(cria_baralho(), indice_carta , Qual_carta)):
-                print (n+1 , item)
-        #if Qual_carta != indice_carta - 3 or Qual_carta != indice_carta - 1:
-            #pergunta_novamente = int(input('Carta invalida, digite uma carta valida:'))
-           # EP2_5.empilha(cria_baralho(), indice_carta , pergunta_novamente)
+        if verifica_funcionamento == [1] :
+            for n, item in enumerate(EP2_5.empilha(cria_baralho(), indice_carta , indice_carta - 1)):
+                print (n + 1, item)
+            
         
-    
+        
+        if verifica_funcionamento == [3]:
+            for n, item in enumerate(EP2_5.empilha(cria_baralho(), indice_carta , indice_carta - 3)):
+                print (n + 1, item)
+            
+        
+        if verifica_funcionamento == [1,3]:
+            Qual_carta = int(input('Qual carta deseja empilhar ?'))
+            while Qual_carta == indice_carta - 3 or Qual_carta == indice_carta - 1 :
+                for n, item in enumerate(EP2_5.empilha(cria_baralho(), indice_carta , Qual_carta)):
+                    print (n+1 , item)
+            #if Qual_carta != indice_carta - 3 or Qual_carta != indice_carta - 1:
+                #pergunta_novamente = int(input('Carta invalida, digite uma carta valida:'))
+            # EP2_5.empilha(cria_baralho(), indice_carta , pergunta_novamente)
+
+else:
+    print ('Fim do jogo')
+    jogar_nv = input('Você deseja jogar novamente ?(s/n) ')
+    if jogar_nv == 's':
+        Restart = True
+    else:
+        Restart = False
+        
+
